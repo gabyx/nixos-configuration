8a9b2d8cf60cf7627aca0f93185824e946677ba0
Gabriel Nützi <gnuetzi@gmail.com>
2023-10-14 12:23:11 +0200

    fix: Typo

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -69,7 +69,7 @@
   ### Networking ==============================================================
   networking.hostName = "gabyx-nixos"; # Define your hostname.
   networking.networkmanager.enable = true;
-  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
