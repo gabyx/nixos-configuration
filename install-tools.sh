99e7a8373bf076cb6f9f4b4b1c4c539ecff6e9f5
Gabriel Nützi <gnuetzi@gmail.com>
2023-10-14 09:54:02 +0200

    fix: Install tools

--- install-tools.sh
+++ install-tools.sh
@@ -11,4 +11,4 @@ set -u
 DIR=$(cd "$(dirname "$0")" && pwd)
 
 sudo git config --system credential.helper ""
-nix-env -iA nixos.git nixos.curl nixos.neovim nixos.wezterm nixos.tmux nixos.clang
+nix-env -iA nixos.gitFull nixos.curl nixos.neovim nixos.wezterm nixos.tmux nixos.clang
