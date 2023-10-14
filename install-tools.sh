72348ebb22b901efdd08aaa4d5ce8e7256d58719
Gabriel Nützi <gnuetzi@gmail.com>
2023-10-14 20:26:45 +0200

    fix: Script

--- install-tools.sh
+++ install-tools.sh
@@ -10,27 +10,26 @@ DIR=$(cd "$(dirname "$0")" && pwd)
 export NIXPKGS_ALLOW_UNFREE=1
 
 nix-env -iA \
-  nixos.gitFull \
-  nixos.curl \
-  nixos.neovim \
-  nixos.wezterm \
-  nixos.tmux \
-  nixos.clang \
-  nixos.google-chrome \
-  nixos.chezmoi
+	nixos.gitFull \
+	nixos.curl \
+	nixos.neovim \
+	nixos.wezterm \
+	nixos.tmux \
+	nixos.clang \
+	nixos.google-chrome \
+	nixos.chezmoi
 
 git config --global user.name "$GIT_USER_NAME"
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global credential.helper "cache --timeout 72000"
 
 if [ "$DOTFILE_ENABLE" = "true" ]; then
-chezmoi init https://github.com/gabyx/chezmoi.git
-chezmoi apply
+	chezmoi init https://github.com/gabyx/chezmoi.git
+	chezmoi apply
 fi
 
 # Install my AstroNvim config
 if [ "$NVIM_ASTRO_ENABLE" = "true" ]; then
-  git clone --depth 1 --branch nightly https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-  git clone "$NVIM_ASTRO_USER_URL" ~/.config/nvim/lua/user
+	git clone --depth 1 --branch nightly https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+	git clone "$NVIM_ASTRO_USER_URL" ~/.config/nvim/lua/user
 fi
-
