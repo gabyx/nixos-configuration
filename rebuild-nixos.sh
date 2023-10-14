739374f29befde6e45aae43776834967e38ff26a
Gabriel Nützi <gnuetzi@gmail.com>
2023-10-14 12:50:06 +0200

    fix: Typo

--- rebuild-nixos.sh
+++ rebuild-nixos.sh
@@ -6,5 +6,7 @@ set -u
 DIR=$(cd "$(dirname "$0")" && pwd)
 . "$DIR/.env"
 
+export NIXPGKS_ALLOW_INSECURE=1
+
 sudo nixos-rebuild -I nixos-config=./configs/nixos/configuration.nix \
   switch -p test
